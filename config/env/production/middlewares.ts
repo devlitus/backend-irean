47395357b8e6814bbed83e7092669e5956552631
devlitus <developercarles@gmail.com>
2025-10-04 06:16:10 +0200

    feat: comentar configuración de middlewares en producción

--- config/env/production/middlewares.ts
+++ config/env/production/middlewares.ts
@@ -1,48 +1,48 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'https://backend-irean-production.up.railway.app',
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'https://backend-irean-production.up.railway.app',
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  {
-    name: 'strapi::session',
-    config: {
-      cookie: {
-        secure: 'auto', // Automatically detect HTTPS from proxy headers
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
-        sameSite: 'lax',
-        signed: true,
-      },
-    },
-  },
-  'strapi::favicon',
-  'strapi::public',
-];
+// export default [
+//   'strapi::logger',
+//   'strapi::errors',
+//   {
+//     name: 'strapi::security',
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           'connect-src': ["'self'", 'https:'],
+//           'img-src': [
+//             "'self'",
+//             'data:',
+//             'blob:',
+//             'market-assets.strapi.io',
+//             'https://backend-irean-production.up.railway.app',
+//           ],
+//           'media-src': [
+//             "'self'",
+//             'data:',
+//             'blob:',
+//             'market-assets.strapi.io',
+//             'https://backend-irean-production.up.railway.app',
+//           ],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   {
+//     name: 'strapi::session',
+//     config: {
+//       cookie: {
+//         secure: 'auto', // Automatically detect HTTPS from proxy headers
+//         httpOnly: true,
+//         maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+//         sameSite: 'lax',
+//         signed: true,
+//       },
+//     },
+//   },
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
