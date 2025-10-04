7dfc922bd7839a4a46cc982d1b1ce6096d2d052b
devlitus <developercarles@gmail.com>
2025-10-04 06:42:19 +0200

    feat: simplificar configuración de admin y eliminar middlewares obsoletos; agregar archivo de configuración para Railway

--- config/env/production/admin.ts
+++ config/env/production/admin.ts
@@ -1,9 +1,6 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
-    options: {
-      expiresIn: '30d',
-    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
@@ -17,7 +14,7 @@ export default ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
-  url: env('STRAPI_ADMIN_BACKEND_URL', 'https://backend-irean-production.up.railway.app/admin'),
+  url: '/admin',
   serveAdminPanel: env.bool('SERVE_ADMIN', true),
   forgotPassword: {
     from: env('EMAIL_FROM', 'no-reply@strapi.io'),
