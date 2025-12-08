92171b51a4810e3413c79b4615c873d9afb12a23
devlitus <developercarles@gmail.com>
2025-12-08 10:17:45 +0100

    feat: agregar logo personalizado y declarar módulos para imágenes en TypeScript

--- src/admin/app.ts
+++ src/admin/app.ts
@@ -1,4 +1,5 @@
 import type { StrapiApp } from '@strapi/strapi/admin';
+import Logo from './extensions/logo_irean.png';
 
 export default {
   config: {
@@ -8,6 +9,12 @@ export default {
           primary500: '#0066cc',
           dark: '#1a1a1a',
         },
+     },
+     auth: {
+       logo: Logo,
+     },
+     menu: {
+       logo: Logo,
      },
   },
   bootstrap(app: StrapiApp) {
